e7cace92251c2e4f15f0156809d3b51dcdf31481
Sven Stähs <staehs.sven@web.de>
2020-11-01 10:24:25 +0100

    partytree: fix party tree bugs that prevented it from growing. Refactored the basic setup, now there's no party tree spot anymore, you just plant the tree in a growing zone like any other. There's also no need for an interactionCell anymore and it gives the proper mood buff upon observation (and a debuff when killed). It has a growing time, hardiness and harvest yield roughly twice that of an oak and can actually be harvested (but won't be harvested automatically).

--- Source/Hobbits/HarmonyHobbits.cs
+++ Source/Hobbits/HarmonyHobbits.cs
@@ -27,48 +27,67 @@ namespace JecsHuntingWithTraps
                 new HarmonyMethod(typeof(HarmonyPatches), nameof(TryFindGatheringSpot_NewTemp_PostFix)), null);
             harmony.Patch(AccessTools.Method(typeof(Plant), "get_Graphic"), null,
                 new HarmonyMethod(typeof(HarmonyPatches), nameof(GetPartyTreeGraphic)), null);
-            harmony.Patch(AccessTools.Method(typeof(CompGlower), "UpdateLit"), null,
-                new HarmonyMethod(typeof(HarmonyPatches), nameof(LightUpPartyTree)), null);
+            harmony.Patch(AccessTools.Method(typeof(CompGlower), "get_ShouldBeLitNow"), null,
+                new HarmonyMethod(typeof(HarmonyPatches), nameof(get_PartyTree_ShouldBeLitNow)), null);
+            harmony.Patch(AccessTools.Method(typeof(WorkGiver_GrowerHarvest), "HasJobOnCell"), null,
+                new HarmonyMethod(typeof(HarmonyPatches), nameof(HasJobOnCell_PostFix)), null);
         }
 
         //CompGlower
-        public static void LightUpPartyTree(CompGlower __instance, Map map)
+        public static void get_PartyTree_ShouldBeLitNow(CompGlower __instance, ref bool __result)
         {
-            if (__instance?.parent is Building_PartyTreeGrower p)
+            if (__instance?.parent is Plant_PartyTree p)
             {
-                if (p?.PlantsOnMe?.First().LifeStage != PlantLifeStage.Mature)
+                if (p.Spawned && p.LifeStage == PlantLifeStage.Mature)
                 {
-                    map.mapDrawer.MapMeshDirty(__instance.parent.Position, MapMeshFlag.Things);
-                    map.glowGrid.DeRegisterGlower(__instance);
+                    __result = true;
                 }
                 else
                 {
-                    map.mapDrawer.MapMeshDirty(__instance.parent.Position, MapMeshFlag.Things);
-                    map.glowGrid.RegisterGlower(__instance);
+                    __result = false;
                 }
             }
         }
 
-        public static Dictionary<Plant, Graphic> treeGraphics;
+        // Do not automatically harvest Party Trees. Player can still designate it explicitly if they wish.
+        public static void HasJobOnCell_PostFix(WorkGiver_GrowerHarvest __instance, Pawn pawn, IntVec3 c, bool forced, ref bool __result)
+        {
+            if (__result == false)
+            {
+                return;
+            }
+            if (c.GetPlant(pawn.Map) is Plant_PartyTree)
+            {
+                __result = false;
+            }
+        }
+
         //Plant
         public static void GetPartyTreeGraphic(Plant __instance, ref Graphic __result)
         {
-            if (__instance.LifeStage == PlantLifeStage.Sowing)
-                return;
-            if (__instance.def.plant.leaflessGraphic != null && __instance.LeaflessNow &&
-                (!__instance.sown || !__instance.HarvestableNow))
-                return;
-            if (__instance.def.defName == "LotRH_PlantPartyTree" &&
-                __instance.LifeStage == PlantLifeStage.Mature)
+            if (!(__instance is Plant_PartyTree))
             {
-                if (treeGraphics == null) treeGraphics = new Dictionary<Plant, Graphic>();
-                if (!treeGraphics.ContainsKey(__instance))
-                {
-                    treeGraphics.Add(__instance,
-                        __instance.def.graphicData.GraphicColoredFor(__instance));
-                    Log.Message("Added tree graphic for : " + __instance.ToString());
-                }
-                __result = treeGraphics[__instance];
+                return;
+            }
+            if (__instance.LifeStage == PlantLifeStage.Sowing)
+            {
+                return;
+            }
+            if (__instance.def.plant.leaflessGraphic != null && __instance.LeaflessNow && (!__instance.sown || !__instance.HarvestableNow))
+            {
+                return;
+            }
+            if (__instance.LifeStage == PlantLifeStage.Mature)
+            {
+                return;
+            }
+
+            // return the immature graphic even if it is harvestable, but not yet fully mature.
+            // only return the mature graphic if it is actually in lifestage "Mature", i.e. lit with lanterns and providing the mood bonus.
+            if (__instance.def.plant.immatureGraphic != null)
+            {
+                __result = __instance.def.plant.immatureGraphic;
+                return;
             }
         }
 
@@ -78,14 +97,10 @@ namespace JecsHuntingWithTraps
             bool enjoyableOutside = JoyUtility.EnjoyableOutsideNow(organizer, null);
             Map map = organizer.Map;
 
-            if (map.listerThings.ThingsOfDef(ThingDef.Named("LotRH_PlantPartyTree")).Any(x => x is Plant y && y.HarvestableNow))
+            if (map.listerThings.ThingsOfDef(ThingDef.Named("LotRH_PlantPartyTree")).Any(x => x is Plant y && y.LifeStage == PlantLifeStage.Mature))
             {
                 bool baseValidator(IntVec3 cell)
                 {
-                    if (!cell.Standable(map))
-                    {
-                        return false;
-                    }
                     if (cell.GetDangerFor(organizer, map) != Danger.None)
                     {
                         return false;
@@ -106,15 +121,15 @@ namespace JecsHuntingWithTraps
                     bool flag = room != null && room.isPrisonCell;
                     return organizer.IsPrisoner == flag && GatheringsUtility.EnoughPotentialGuestsToStartGathering(map, GatheringDefOf.Party, new IntVec3?(cell));
                 }
-                if ((from x in map.listerThings.ThingsOfDef(ThingDef.Named("LotRH_PlantPartyTree")).Where(x => x is Plant y && y.HarvestableNow)
-                    where baseValidator(x.InteractionCell)
-                    select x.InteractionCell).TryRandomElement(out result))
+                if ((from x in map.listerThings.ThingsOfDef(ThingDef.Named("LotRH_PlantPartyTree")).Where(x => x is Plant y && y.LifeStage == PlantLifeStage.Mature)
+                     where baseValidator(x.Position)
+                     select x.Position).TryRandomElement(out result))
                 {
                     __result = true;
                 }
             }
         }
-        
+
         public static void CanHaveSnow_PostFix(SnowGrid __instance, int ind, ref bool __result)
         {
             var map = Traverse.Create(__instance).Field("map").GetValue<Map>();
